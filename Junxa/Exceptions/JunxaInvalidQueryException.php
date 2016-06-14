0b0b9b259dd12d59cc3da9657380ac4c7fdb7472
Matthew Sheahan <chaos@thaumatic.com>
2016-06-14 03:18:27 -0400

    add invalid query exception

---
+++ Junxa/Exceptions/JunxaInvalidQueryException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Thaumatic\Junxa\Exceptions;
+
+/**
+ * Class for exceptions arising from an invalid query specification.
+ */
+class JunxaInvalidQueryException
+	extends JunxaException
+{
+}
