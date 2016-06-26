ca2cfa38ae856ef661bf562749b6bfa799aa8c72
Matthew Sheahan <chaos@thaumatic.com>
2016-06-26 02:50:04 -0400

    add no-such-column exception

---
+++ Junxa/Exceptions/JunxaNoSuchColumnException.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Thaumatic\Junxa\Exceptions;
+
+/**
+ * Exception class for requests for a nonexistent column.
+ */
+class JunxaNoSuchColumnException
+	extends JunxaException
+{
+
+	private $columnName;
+
+	public function __construct($columnName)
+	{
+		$this->columnName = $columnName;
+		parent::__construct('no such column: ' . $columnName);
+	}
+
+	public function getColumnName()
+	{
+		return $this->columnName;
+	}
+
+}
