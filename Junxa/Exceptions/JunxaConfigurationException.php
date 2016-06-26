b195d7c58b2de24f2d2e14f20b9c124470b714d8
Matthew Sheahan <chaos@thaumatic.com>
2016-06-25 22:54:55 -0400

    add configuration exception

---
+++ Junxa/Exceptions/JunxaConfigurationException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Thaumatic\Junxa\Exceptions;
+
+/**
+ * Class for exceptions arising from invalid configuration.
+ */
+class JunxaConfigurationException
+	extends JunxaException
+{
+}
