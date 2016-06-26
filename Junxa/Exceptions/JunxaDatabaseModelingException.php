273af77f6a561974459b2f38a4eb2855ce3a98ce
Matthew Sheahan <chaos@thaumatic.com>
2016-06-26 13:05:11 -0400

    add database modeling exception

---
+++ Junxa/Exceptions/JunxaDatabaseModelingException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Thaumatic\Junxa\Exceptions;
+
+/**
+ * Exception class for apparent failures to model the underlying database.
+ */
+class JunxaDatabaseModelingException
+	extends JunxaException
+{
+}
