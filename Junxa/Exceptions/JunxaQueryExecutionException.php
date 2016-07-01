91bace23e9c671db8d02c82316fb0fed5c993657
Matthew Sheahan <chaos@thaumatic.com>
2016-07-01 00:44:38 -0400

    add JunxaQueryExecutionException

---
+++ Junxa/Exceptions/JunxaQueryExecutionException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Thaumatic\Junxa\Exceptions;
+
+/**
+ * Class for exceptions arising from failed query execution.
+ */
+class JunxaQueryExecutionException
+	extends JunxaException
+{
+}
