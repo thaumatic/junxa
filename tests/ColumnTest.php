a1bf895df6e1dff983ba327ce15a5c6ca08088fb
Matthew Sheahan <chaos@thaumatic.com>
2016-07-10 03:19:07 -0400

    expand column tests

--- tests/ColumnTest.php
+++ tests/ColumnTest.php
@@ -50,6 +50,24 @@ class ColumnTest extends DatabaseTestAbstract
         $this->assertNull($categoryIdColumn->getDefault());
         $this->assertFalse($categoryIdColumn->hasDefault());
         $this->assertNull($categoryIdColumn->getDefaultValue());
+        $categoryNameColumn = $db->category->name;
+        $this->assertInstanceOf('Thaumatic\Junxa\Column', $categoryNameColumn);
+        $this->assertSame('name', $categoryNameColumn->getName());
+        $this->assertSame('varchar(250)', $categoryNameColumn->getFullType());
+        $this->assertSame('varchar', $categoryNameColumn->getType());
+        $this->assertSame('text', $categoryNameColumn->getTypeClass());
+        $this->assertSame(
+            Column::MYSQL_FLAG_NOT_NULL |
+            Column::MYSQL_FLAG_UNIQUE_KEY |
+            Column::MYSQL_FLAG_NO_DEFAULT_VALUE |
+            Column::MYSQL_FLAG_PART_KEY,
+            $categoryNameColumn->getFlags()
+        );
+        $this->assertFalse($categoryNameColumn->isDynamic());
+        $this->assertNull($categoryNameColumn->getDynamicAlias());
+        $this->assertNull($categoryNameColumn->getDefault());
+        $this->assertFalse($categoryNameColumn->hasDefault());
+        $this->assertNull($categoryNameColumn->getDefaultValue());
     }
 
 }
