b37b9892a66ac8cd1d074431210b00f45fd0bbe0
Matthew Sheahan <chaos@thaumatic.com>
2016-07-31 01:47:41 -0400

    add internal inconsistency exception

---
+++ Junxa/Exceptions/JunxaInternalInconsistencyException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Thaumatic\Junxa\Exceptions;
+
+/**
+ * Exception class for error conditions that should not be reachable.
+ */
+class JunxaInternalInconsistencyException extends JunxaException
+{
+}
