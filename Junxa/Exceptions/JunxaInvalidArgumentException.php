c8a893d5d83da0b8a68679406e532ed3c5c3cc5d
Matthew Sheahan <chaos@thaumatic.com>
2016-08-06 03:04:04 -0400

    add JunxaInvalidArgumentException

---
+++ Junxa/Exceptions/JunxaInvalidArgumentException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Thaumatic\Junxa\Exceptions;
+
+/**
+ * Class for exceptions arising from an invalid method argument.
+ */
+class JunxaInvalidArgumentException extends JunxaException
+{
+}
