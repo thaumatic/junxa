4240a3e3b97e71914c39c8899f3d7be72f9a911c
Matthew Sheahan <chaos@thaumatic.com>
2016-08-22 02:36:11 -0400

    suppress coding standards complaints about test class name pattern

--- tests/Row/Category/_2ndCategory.php
+++ tests/Row/Category/_2ndCategory.php
@@ -2,11 +2,13 @@
 
 namespace Thaumatic\Junxa\Tests\Row\Category;
 
+// @codingStandardsIgnoreStart
 class _2ndCategory extends \Thaumatic\Junxa\Tests\Row\Category
 {
 
     public function isTest_2ndCategoryCategoryRow()
     {
+        // @codingStandardsIgnoreEnd
         return true;
     }
 
