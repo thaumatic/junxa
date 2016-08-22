cd63e44adc64fecce1d3fa3476d90941c874ea0c
Matthew Sheahan <chaos@thaumatic.com>
2016-08-21 20:17:24 -0400

    expand test database setup

--- tests/test.sql
+++ tests/test.sql
@@ -25,3 +25,29 @@ CREATE TABLE `item` (
     FOREIGN KEY `categoryId` (`categoryId`) REFERENCES `category` (`id`),
     KEY `categoryIdByActive` (`categoryId`, `active`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `tag`;
+CREATE TABLE `tag` (
+    `id` mediumint unsigned not null auto_increment,
+    `name` varchar(250) not null,
+    `type` enum('A''s', 'B''s', 'C''s'),
+    `active` bool not null default 1,
+    `createdAt` datetime not null,
+    `changedAt` timestamp not null,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `name` (`name`),
+    KEY `active` (`active`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `itemTag`;
+CREATE TABLE `item_tag` (
+    `id` mediumint unsigned not null auto_increment,
+    `itemId` mediumint unsigned not null,
+    `tagId` mediumint unsigned not null,
+    `active` bool not null default 1,
+    `createdAt` datetime not null,
+    `changedAt` timestamp not null,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `itemIdTagId` (`itemId`,`tagId`),
+    KEY `active` (`active`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
