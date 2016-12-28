4c82e39604e7a5d1f2600fffc5afc262397e9b99
Matthew Sheahan <chaos@thaumatic.com>
2016-12-28 04:46:55 -0500

    built out initial use case for validation framework

---
+++ Junxa/Validation/Rule/Required.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Thaumatic\Junxa\Validation\Rule;
+
+use Thaumatic\Junxa\Column;
+use Thaumatic\Junxa\Validation\RuleAbstract;
+
+/**
+ * Validation rule for required fields.
+ */
+class Required extends RuleAbstract
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFailureMessageFormat()
+    {
+        return '%%NAME_WITH_ARTICLE%% is required';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validate(Column $column, &$value, $data, $prefix, $suffix)
+    {
+        return $value != '';
+    }
+
+    public function getJavascriptFailureConditions(Column $column, $refer, $args = [])
+    {
+        return [
+            $refer . '.value == \'\'',
+        ];
+    } 
+
+}
