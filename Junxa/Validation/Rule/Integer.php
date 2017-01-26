ff2b6d25ad3561a850507e230cfa8e3a16d3049e
Matthew Sheahan <chaos@thaumatic.com>
2017-01-26 01:21:02 -0500

    add Integer validation rule

---
+++ Junxa/Validation/Rule/Integer.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Thaumatic\Junxa\Validation\Rule;
+
+use Thaumatic\Junxa\Column;
+use Thaumatic\Junxa\Validation\RuleAbstract;
+
+/**
+ * Validation rule for integer fields.
+ */
+class Integer extends RuleAbstract
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFailureMessageFormat()
+    {
+        return '%%NAME%% must be a number (with no decimals)';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validate(Column $column, &$value, $data, $prefix, $suffix)
+    {
+        return strval(intval($value)) === $value;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getJavascriptFailureConditions(Column $column, $refer, $args = [])
+    {
+        return [
+            'Number.parseInt(' . $refer . '.value) == ' . $refer . '.value',
+        ];
+    }
+
+}
