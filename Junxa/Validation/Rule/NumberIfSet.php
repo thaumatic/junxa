69cde571e9f984ccc4cf11634093e0e0b76d14ec
Matthew Sheahan <chaos@thaumatic.com>
2017-01-26 01:35:24 -0500

    add NumberIfSet validation rule

---
+++ Junxa/Validation/Rule/NumberIfSet.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Thaumatic\Junxa\Validation\Rule;
+
+use Thaumatic\Junxa\Column;
+use Thaumatic\Junxa\Validation\RuleAbstract;
+
+/**
+ * Validation rule for numeric fields that may be left unset.
+ */
+class Number extends RuleAbstract
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFailureMessageFormat()
+    {
+        return '%%NAME%% must be a number or empty';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validate(Column $column, &$value, $data, $prefix, $suffix)
+    {
+        return $value === '' || $value === null || filter_var($value, FILTER_VALIDATE_FLOAT);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getJavascriptFailureConditions(Column $column, $refer, $args = [])
+    {
+        return [
+            $refer
+            . '.value !== \'\' && (isNaN(Number.parseFloat('
+            . $refer
+            . '.value)) || !Number.isFinite('
+            . $refer
+            . '.value))',
+        ];
+    }
+
+}
